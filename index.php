d75c63096bd30553b09dcdae716af52b24663e75
gargantua892 <94129392+And18892@users.noreply.github.com>
2023-07-03 14:39:42 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <?php 
+
+        require_once("./data/db.php");
+
+    ?>
+
+    <!-- <title>User: <?php echo ?></title> mettiamo titolo categoria -->
+</head>
+<body>
+</body>
+</html>
