5192b1098646d07e8652512d2e9eee276153ad44
gargantua892 <94129392+And18892@users.noreply.github.com>
2023-07-04 15:59:08 +0200

    esercizio corretto

--- index.php
+++ index.php
@@ -3,15 +3,59 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>E-commerce</title>
 
-    <?php 
+    <?php require_once("./data/db.php"); ?>
 
-        require_once("./data/db.php");
+</head>
+<body>
+    
+    <h2>Food</h2>
+
+    <?php
+
+        foreach ($foods as $food) {
+            
+            ?> 
+            
+                <div>
+                    <h3>
+                        [<?php echo $food -> getCategory() -> getName(); ?>]
+                        <?php echo $food -> getName(); ?>
+                    </h3>
+                    <div>Peso: <?php echo $food -> getWeight() ?>Kg</div>
+                    <div>Prezzo: <?php echo $food -> getPrice() ?>E</div>
+                    <div>Data scadenza: <?php echo $food -> getExpireDate() ?></div>
+                </div>
+            
+            <?php
+        }
 
     ?>
 
-    <!-- <title>User: <?php echo ?></title> mettiamo titolo categoria -->
-</head>
-<body>
+    <h2> Toys </h2>
+
+    <?php
+
+        foreach ($toys as $toy) {
+            
+            ?> 
+            
+                <div>
+                    <h3>
+                        [<?php echo $toy -> getCategory() -> getName(); ?>]
+                        <?php echo $toy -> getName(); ?>
+                    </h3>
+                    <div>Peso: <?php echo $toy -> getWeight() ?>Kg</div>
+                    <div>Prezzo: <?php echo $toy -> getPrice() ?>E</div>
+                    <div>Colore: <?php echo $toy -> getColor() ?></div>
+                    <div>Eta' consigliata: <?php echo $toy -> getAge() ?></div>
+                </div>
+            
+            <?php
+        }
+
+    ?>
+
 </body>
 </html>
